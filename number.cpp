32675e181505710b23286e681463d3bc1e28968b
Gusti Mufid <107171831+gstmfd@users.noreply.github.com>
2024-05-09 10:23:56 +0700

    Add files via upload

---
+++ number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int number;
+    cin >> number;
+    
+    while(number >= 0){
+      cout << number << endl;
+      number--;
+    }
+    return 0;
+}
