0e10459e0b5bf6fcf82b894a5a67324af52e4d72
Gusti Mufid <107171831+gstmfd@users.noreply.github.com>
2024-06-30 17:58:40 +0700

    Add files via upload

---
+++ apLahh.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    double number = 80.2;
+    double numbers = 19.8;
+    
+    cout << number + numbers;
+}
