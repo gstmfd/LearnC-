a6e19223289607965a8d6b95ea12d84d112ade60
Gusti Mufid <107171831+gstmfd@users.noreply.github.com>
2024-07-03 18:14:22 +0700

    Add files via upload

---
+++ opopoo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama, jurusan, kelas;
+    int umur;
+
+    cout << " Masukan Nama: ";
+    cin >> nama;
+    std::cout << endl;
+    cout << " Masukan Jurusan: ";
+    cin >> jurusan;
+    std::cout << endl;
+    cout << " Masukan Kelas: ";
+    cin >> kelas;
+    std::cout << endl;
+    cout << " Masukan Umur: ";
+    cin >> umur;
+    std::cout << endl;
+}
